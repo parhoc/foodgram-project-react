eea2bff572d16755fde3c721ce354b0c7a593be1
Konstantin Parhoc <parhockonstantin@gmail.com>
2024-01-15 14:38:34 +0300

    add UserViewSet and user registration

---
+++ backend/api/serializers.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        write_only=True
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+        )
+        read_only_fields = (
+            'id',
+        )
