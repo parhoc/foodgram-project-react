a66c9caaafdfd463f2dd00d687831ff821911df3
Konstantin Parhoc <parhockonstantin@gmail.com>
2024-01-18 14:25:07 +0300

    add Tag, Recipe, Ingredient models

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,3 +1,70 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    Tag,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'color',
+        'slug',
+    )
+    list_filter = (
+        'color',
+    )
+    search_fields = (
+        'name',
+        'slug',
+    )
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'measurement_unit',
+    )
+    list_filter = (
+        'measurement_unit',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'author',
+    )
+    list_filter = (
+        'author',
+        'name',
+        'tags',
+    )
+    search_fields = (
+        'name',
+    )
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'recipe',
+        'ingredient',
+    )
+    list_filter = (
+        'recipe',
+        'ingredient'
+    )
+    search_fields = (
+        'recipe',
+        'ingredient'
+    )
--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,3 +1,130 @@
+import re
+
+from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator, RegexValidator
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    HEX_COLOR_REGEX = re.compile(r'^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$')
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    color = models.CharField(
+        verbose_name='Цвет',
+        max_length=7,
+        validators=(
+            RegexValidator(HEX_COLOR_REGEX),
+        )
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        unique=True
+    )
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+        ordering = (
+            'name',
+        )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        verbose_name='Единицы измерения',
+        max_length=200
+    )
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = (
+            'name',
+        )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=200
+    )
+    text = models.TextField(
+        verbose_name='Описание'
+    )
+    ingredients = models.ManyToManyField(
+        Ingredient,
+        through='RecipeIngredient',
+        verbose_name='Ингредиетны',
+        related_name='recipes'
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        verbose_name='Теги',
+        related_name='recipes'
+    )
+    image = models.ImageField(
+        verbose_name='Картинка',
+        upload_to='recipes/images/'
+    )
+    cooking_time = models.IntegerField(
+        verbose_name='Время приготовления, мин',
+        validators=(
+            MinValueValidator(1),
+        )
+    )
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор',
+        on_delete=models.CASCADE,
+        related_name='recipes'
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = (
+            'name',
+        )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        verbose_name='Рецепт'
+    )
+    ingredient = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        verbose_name='Ингредиент'
+    )
+    amount = models.IntegerField(
+        verbose_name='Количество',
+        validators=(
+            MinValueValidator(1),
+        )
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт и ингредиент'
+        verbose_name_plural = 'Рецепты и ингредиенты'
+
+    def __str__(self):
+        return f'{self.recipe}, {self.ingredient}'
