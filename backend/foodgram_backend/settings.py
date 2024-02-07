4369c98973f5389911479d07d0bfd7ea01229151
Konstantin Parhoc <parhockonstantin@gmail.com>
2024-02-07 14:34:03 +0300

    add hosts

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = os.getenv('DEBUG', True)
 
-ALLOWED_HOSTS = [os.getenv('HOST', '127.0.0.1')]
+ALLOWED_HOSTS = os.getenv('HOST').split()
 
 
 INSTALLED_APPS = [
