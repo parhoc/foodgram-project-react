7a18fbf80d8bd86d9d2799f0a7ac6227b1ea947a
Konstantin Parhoc <parhockonstantin@gmail.com>
2024-02-14 13:31:57 +0300

    spread separately users and recipes api

--- backend/api/recipes/views.py
+++ backend/api/recipes/views.py
@@ -1,151 +1,17 @@
-from django.contrib.auth import get_user_model
+from api import utils
+from api.permissions import IsAuthorAdminOrReadOnly
+from api.serializers import (FavoriteSerializer, IngredientSerializer,
+                             RecipeCreateSerializer, RecipeSerializer,
+                             ShoppingCartSerializer, TagSerializer)
 from django.http import FileResponse, Http404
 from django_filters.rest_framework import DjangoFilterBackend
-from djoser.serializers import SetPasswordSerializer
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            ShoppingCart, Subscription, Tag)
+                            ShoppingCart, Tag)
 from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from . import utils
 from .filters import IngredientFilter, RecipeFilter
-from .permissions import IsAuthorAdminOrReadOnly
-from .serializers import (CustomUserCreateSerializer, CustomUserSerializer,
-                          FavoriteSerializer, IngredientSerializer,
-                          RecipeCreateSerializer, RecipeSerializer,
-                          ShoppingCartSerializer, SubscriptionSerializer,
-                          TagSerializer)
-
-User = get_user_model()
-
-
-class CustomUserViewSet(mixins.CreateModelMixin, mixins.ListModelMixin,
-                        mixins.RetrieveModelMixin, viewsets.GenericViewSet):
-    """
-    User model ViewSet.
-
-    Include create, list and retrive generic methods.
-
-    Methods
-    -------
-    me
-        Reference to current user.
-    subscriptions
-        Current user subscriptions.
-    set_password
-        Change current user password.
-    subscribe
-        Add or remove subscription to specified user.
-    """
-
-    queryset = User.objects.all()
-    serializer_class = CustomUserSerializer
-
-    def get_instance(self):
-        return self.request.user
-
-    def get_serializer_class(self):
-        if self.action == 'create':
-            return CustomUserCreateSerializer
-        if self.action == 'set_password':
-            return SetPasswordSerializer
-        if self.action in ('subscribe', 'subscriptions'):
-            return SubscriptionSerializer
-        return CustomUserSerializer
-
-    @action(
-        ['get'],
-        detail=False,
-        permission_classes=(permissions.IsAuthenticated,)
-    )
-    def me(self, request, *args, **kwargs):
-        """
-        Return response with current authenticated user details.
-
-        Get method. Availible only to authenticated users.
-        """
-        self.get_object = self.get_instance
-        return self.retrieve(request, *args, **kwargs)
-
-    @action(
-        ['get'],
-        detail=False,
-        permission_classes=(permissions.IsAuthenticated,)
-    )
-    def subscriptions(self, request):
-        """
-        Return response with current authenticated user subscriptions.
-
-        Get method. Availible only to authenticated users.
-        """
-        queryset = request.user.subscriptions.all()
-        page = self.paginate_queryset(queryset)
-        serializer = self.get_serializer(
-            page,
-            many=True,
-            context={
-                'request': request,
-            }
-        )
-        return self.get_paginated_response(serializer.data)
-
-    @action(
-        ['post'],
-        detail=False,
-        permission_classes=(permissions.IsAuthenticated,)
-    )
-    def set_password(self, request):
-        """
-        Change current authenticated user password.
-
-        Post method. Availible only to authenticated users.
-        """
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        self.request.user.set_password(serializer.data["new_password"])
-        self.request.user.save()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    def create_subscription(self, user, subscription):
-        data = {
-            'user': user.pk,
-            'subscription': subscription.pk,
-        }
-        serializer = self.get_serializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-
-    def destroy_subscription(self, user, subscription):
-        try:
-            instance = user.subscriptions.get(subscription=subscription)
-        except Subscription.DoesNotExist:
-            return Response(
-                {'errors': 'Subscription does not exist.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        instance.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    @action(
-        ['post', 'delete'],
-        detail=True,
-        permission_classes=(permissions.IsAuthenticated,)
-    )
-    def subscribe(self, request, pk):
-        """
-        Create or delete subscription for current user to specified user.
-
-        Post and delete method. Availible only to authenticated users.
-        """
-        subscription = self.get_object()
-        if request.method == 'POST':
-            return self.create_subscription(request.user, subscription)
-        if request.method == 'DELETE':
-            return self.destroy_subscription(request.user, subscription)
 
 
 class TagViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
